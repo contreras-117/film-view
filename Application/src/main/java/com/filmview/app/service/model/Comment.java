5f68c29e61b7344f12d56531f2e90ead6bada64f
Armando Contreras <mandoco.97@gmail.com>
2023-10-08 14:42:45 -0700

    Commit model setup

--- Application/src/main/java/com/filmview/app/service/model/Comment.java
+++ Application/src/main/java/com/filmview/app/service/model/Comment.java
@@ -3,17 +3,53 @@ package com.filmview.app.service.model;
 import static java.util.UUID.randomUUID;
 
 public class Comment {
-    String originalComment;
+    final String originalComment;
     String editedComment;
     String commentId;
-    User username;
+    User user;
 
-    public Comment(String comment, User username) {
+    public Comment(String comment, User user) {
         this.originalComment = comment;
-        this.username = username;
+        this.user = user;
         this.editedComment = "";
         this.commentId = randomUUID().toString();
     }
 
+    public String getOriginalComment() {
+        return originalComment;
+    }
 
+    public String getEditedComment() {
+        return editedComment;
+    }
+
+    public void setEditedComment(String editedComment) {
+        this.editedComment = editedComment;
+    }
+
+    public String getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(String commentId) {
+        this.commentId = commentId;
+    }
+
+    public User getUsername() {
+        return user;
+    }
+
+    public void setUsername(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "originalComment='" + originalComment + '\'' +
+                ", editedComment='" + editedComment + '\'' +
+                ", commentId='" + commentId + '\'' +
+                ", user=" + user.username +
+                '}';
+    }
 }
